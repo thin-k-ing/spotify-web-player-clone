5c33746e13a06ba4bc842ef9bf9f383ec8b2d489
Rishabh Kumar Soni <rishabh.soni@orchids.edu.in>
2022-01-24 10:21:12 +0530

    minor fixes

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-const BASE_URL = "https://thinking-spotify-server-v1.herokuapp.com";
+const BASE_URL = "https://confident-boyd-50f277.netlify.app";
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=60edc3391b794b7aa3fe52f5a8ef6511&response_type=code&redirect_uri=${BASE_URL}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 export default function Login() {
